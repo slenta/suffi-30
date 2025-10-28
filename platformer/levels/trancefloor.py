9aff3929b00098a48f5c7fcecd41aa0e14d9087a
David <dajac@posteo.de>
2025-10-28 15:36:28 +0100

    mini change

--- platformer/levels/trancefloor.py
+++ platformer/levels/trancefloor.py
@@ -209,7 +209,7 @@ level_config = {
             "chase_range": 10,
             "melee_damage": 2,
             "can_throw_explosives": False,
-            "can_summon_minions": True,  # ← Add this line to enable minion spawning
+            "can_summon_minions": False,  # ← Add this line to enable minion spawning
         },
         {
             "x": 140,
